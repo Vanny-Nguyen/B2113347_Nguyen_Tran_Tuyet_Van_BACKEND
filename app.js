6e9d270139f49eb71c42322f98a30070803eca02
Vanny <vanb2113347@student.ctu.edu.vn>
2024-09-20 19:51:27 +0700

    Cai dat xu  ly loi

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
+
+const ApiError = require("./app/api-error");
+
 const app = express();
 
-const contactsRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactsRouter);
+
+//handle 404 respone
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +27,5 @@ app.get("/", (req, res) => {
     res.json ({message: "Welcome to contact book application."});
 });
 
-app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
