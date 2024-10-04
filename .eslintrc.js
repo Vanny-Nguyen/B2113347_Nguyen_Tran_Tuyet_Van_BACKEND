54188f03415265045b4be90a83ef24366d289af7
Vanny <vanb2113347@student.ctu.edu.vn>
2024-10-04 17:35:48 +0700

    Cai dat cac handler truy xuat CSDL

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     env: {
-        node: true,
+        node: true, 
         commonjs: true,
         es2021: true,
     },
-    extends: ['eslint:recommended', 'prettier'],
+    extends: ['eslint: recommended', 'prettier'],
 };
--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 const express = require("express");
 const cors = require("cors");
-
 const contactsRouter = require("./app/routes/contact.route");
-
-const ApiError = require("./app/api-error");
+const ApiError = require("./app/api-error")
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
-//handle 404 respone
+//handel 404 respond
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
-});
+})
 
+//define error-handling middleware last, after other app.use() and routes calls 
 app.use((err, req, res, next) => {
     return res.status(err.statusCode || 500).json({
-        message: err.message || "Internal Server Error",
+        message: err.message || "Internal Server Error"
     });
 });
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json ({message: "Welcome to contact book application."});
+app.get("/", (rep, res) => {
+    res.json({message: "Welcome to contact book application."});
 });
-
 
 module.exports = app;
